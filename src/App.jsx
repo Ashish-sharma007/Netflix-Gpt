3c6cf9e8de8b5d56a265831496ea58db322310c8
Ashish Sharma <ashishsharma47@hotmail.com>
2023-10-26 13:07:08 +0530

    Netflix Login/Signup screen

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import DonutChart from 'react-donut-chart';
+
+import Body from './Body';
+
+function Donut() {
+
+
+  return (
+    <>
+    <Body/>
+ </>
+  );
+}
+
+export default Donut;
